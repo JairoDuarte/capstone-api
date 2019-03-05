8ba7201b3e3a8737d94c2b0a53de3b36fd26f895
Jairo Duarte <alfredojairo17@hotmail.com>
2019-03-05 17:39:33 +0100

    docker update for added google chrome because signup with facebook test need one browser. test for show me and update coursier status added

--- test/integration/setup.js
+++ test/integration/setup.js
@@ -2,7 +2,6 @@ import { EventEmitter } from 'events'
 import supertest from 'supertest';
 import Enzyme from 'enzyme';
 import "@babel/polyfill";
-import auth from './token';
 
 import app from '../../src/app.js';
 
@@ -15,10 +14,10 @@ beforeAll(async () => {
   global.app = app;
   global.supertest = supertest;
   global.request = await supertest(app);
-  //await auth.auth('coursier');
-  await auth.auth('customer');
   
-  global.customer = auth.getcustomerToken();
-  global.coursier = auth.getCoursierToken();
+  global.customer = { token:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVjN2VhNTA2NDJjOTNiYmMyMjQ4OTViMyIsImV4cGlyZVRva2VuIjoxMDAwMDAwMDE1NTE0MDg0NjAwMDAsImlhdCI6MTU1MTgwMzY1NH0.Q9hsp7F22hzVmlPxt7rnMDRqlPPYw4ZhBrHv0IxGfIc',
+   user: { id: '5c7ea50642c93bbc224895b3', role: 'customer' },
+   done: true }
   
 })
